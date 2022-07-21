1effc4fd40edc5fb23a2ae880c6eb7cb5a261d89
ldali42 <ombellinedali@gmail.com>
2022-07-21 14:49:58 +0200

    ex02 modified

--- copy_du_c/c05notlink/ex02/ft_iterative_power.c
+++ copy_du_c/c05notlink/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: ldalibar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 07:35:59 by ldalibar          #+#    #+#             */
-/*   Updated: 2022/07/21 14:19:54 by ldalibar         ###   ########lyon.fr   */
+/*   Updated: 2022/07/21 14:48:51 by ldalibar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_iterative_power(int nb, int power)
 	int	result;
 
 	i = 1;
-	if (power < 0)
+	if (power < 0 || nb < 0)
 		return (0);
 	if ((power == 0 && nb == 0) || power == 0)
 		return (1);
