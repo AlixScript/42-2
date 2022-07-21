aa7caa601c7611079623f9be4af45170a4aa6f11
ldali42 <ombellinedali@gmail.com>
2022-07-21 09:37:19 +0200

    1

--- copy_du_c/c05notlink/ex00/ft_iterative_factorial.c
+++ copy_du_c/c05notlink/ex00/ft_iterative_factorial.c
@@ -6,16 +6,23 @@
 /*   By: ldalibar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 07:33:39 by ldalibar          #+#    #+#             */
-/*   Updated: 2022/07/20 07:34:20 by ldalibar         ###   ########lyon.fr   */
+/*   Updated: 2022/07/21 09:22:55 by ldalibar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_iterative_factorial(int nb)
+int	ft_iterative_factorial(int nb)
 {
+	int	i;
+	int	result;
 
-}
-
-int main()
-{
-
+	i = 2;
+	result = 1;
+	if (nb <= 0 || nb > 12)
+		return (0);
+	while (i <= nb)
+	{
+		result = result * i;
+		i++;
+	}
+	return (result);
 }
--- copy_du_c/c05notlink/ex01/ft_recursive_factorial.c
+++ copy_du_c/c05notlink/ex01/ft_recursive_factorial.c
@@ -6,16 +6,35 @@
 /*   By: ldalibar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 07:34:47 by ldalibar          #+#    #+#             */
-/*   Updated: 2022/07/20 07:35:25 by ldalibar         ###   ########lyon.fr   */
+/*   Updated: 2022/07/21 09:37:00 by ldalibar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int ft_recursive_factorial(int nb)
 {
+	int i;
+	int result;
 
+	i = 2;
+	result = 1;
+	if (nb <= 0 || nb > 12)
+		return 0;
+	if (i <= nb)
+		ft_recursive_factorial(result = i * result);
+		//i++;
+	/*
+	while (i <= nb)
+	{
+		result = i * result;
+		i++;
+	}*/
+	return (result);
 }
 
 int main()
 {
-
+	int nb = 5;
+	printf("%d", ft_recursive_factorial(nb));
 }
