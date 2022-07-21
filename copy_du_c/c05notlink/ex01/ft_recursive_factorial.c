1e1e8df26835cec2d242b8355fe245aa7dba3272
ldali42 <ombellinedali@gmail.com>
2022-07-21 13:01:01 +0200

    ex00 and 01 add

--- copy_du_c/c05notlink/ex01/ft_recursive_factorial.c
+++ copy_du_c/c05notlink/ex01/ft_recursive_factorial.c
@@ -6,35 +6,15 @@
 /*   By: ldalibar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 07:34:47 by ldalibar          #+#    #+#             */
-/*   Updated: 2022/07/21 09:37:00 by ldalibar         ###   ########lyon.fr   */
+/*   Updated: 2022/07/21 11:33:35 by ldalibar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int ft_recursive_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
-	int i;
-	int result;
-
-	i = 2;
-	result = 1;
 	if (nb <= 0 || nb > 12)
-		return 0;
-	if (i <= nb)
-		ft_recursive_factorial(result = i * result);
-		//i++;
-	/*
-	while (i <= nb)
-	{
-		result = i * result;
-		i++;
-	}*/
-	return (result);
-}
-
-int main()
-{
-	int nb = 5;
-	printf("%d", ft_recursive_factorial(nb));
+		return (0);
+	if (nb > 1)
+		return (nb * ft_recursive_factorial(nb - 1));
+	return (nb);
 }
